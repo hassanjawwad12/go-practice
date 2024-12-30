9e78eab6e42c6d78ccbc4d5cf1357a3564970b85
Hassan Jawwad <hassan.jawwad@datumbrain.com>
2024-12-30 17:05:20 +0500

    add rune data type

--- arrays/main.go
+++ arrays/main.go
@@ -1,34 +1,32 @@
 package main
-import "fmt"  
+
+import "fmt"
 
 //array is a fixed length sequence of elements of a specific type
-func main () {
+func main() {
 
-    var nums [5]int=[5]int{1,2,3,4,5}
+	var nums [5]int = [5]int{1, 2, 3, 4, 5}
 	fmt.Println(nums)
 	fmt.Println(len(nums))
 
-	var numbers [4]int;
+	var numbers [4]int
 
-	numbers[0]=1 
-	numbers[1]=2
-	numbers[2]=3
+	numbers[0] = 1
+	numbers[1] = 2
+	numbers[2] = 3
 	//numbers[3]=4
 	//it will print 0 for the indexes which are not assigned any integer value
 	fmt.Println(numbers)
 
-	var vals[4]bool; 
-	vals[2]=true
+	var vals [4]bool
+	vals[2] = true
 	fmt.Println(vals)
 
 	//simpler syntax
-	prices:=[3]float64{1.1,2.2,3.3}
+	prices := [3]float64{1.1, 2.2, 3.3}
 	fmt.Println(prices[2])
 
-	//2D arrays 
-	two:= [2][3]int{{1,2,3},{4,5,6}}
+	//2D arrays
+	two := [2][3]int{{1, 2, 3}, {4, 5, 6}}
 	fmt.Println(two)
-
-
 }
-
